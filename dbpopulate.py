f71fcf2ca288dc00065e271fc8d6e364f630ea60
PaulBeardsley <108080617+PaulBeardsley@users.noreply.github.com>
2024-08-15 15:09:02 +0100

    Update dbpopulate.py

--- dbpopulate.py
+++ dbpopulate.py
@@ -4,11 +4,11 @@ con = sl.connect('my-test.db')
 
 sql = 'INSERT INTO USER (id, name, age) values(?, ?, ?)'
 data = [
-    (1, 'Alice', 21),
+    (1, 'Yvonne', 21),
     (2, 'Bob', 22),
-    (3, 'Chris', 23),
+    (3, 'Aaron', 23),
     (4, 'Daphne', 40),
-    (5, 'Edith', 12),
+    (5, 'Alice', 12),
     (6, 'Fiona', 22)
 ]
 
